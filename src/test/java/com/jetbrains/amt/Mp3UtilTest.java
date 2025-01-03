163b307b9be8f02bd7c663e6eaab68afcaffa3ab
Aung Myat Thu <bororo.vox@gmail.com>
2025-01-03 22:38:45 +0630

    Initial commit.

---
+++ src/test/java/com/jetbrains/amt/Mp3UtilTest.java
@@ -0,0 +1,15 @@
+package com.jetbrains.amt;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+public class Mp3UtilTest {
+
+    @Test
+    void getArtist_shouldBe_KraftClub() {
+        String artist = Mp3Util.getArtist(Paths.get("./my.mp3"));
+        Assertions.assertEquals("Aung Myat Thu", artist);
+    }
+}
